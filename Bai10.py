0154f62e9aa7c70e22ce4be872ead85065871eed
inugato <longlamloi97@gmail.com>
2017-09-28 09:59:15 +0700

    updated

---
+++ Bai10.py
@@ -0,0 +1,6 @@
+import math
+i = 1
+x = input('Input x: ')
+y = input('Input y: ')
+power= math.pow(x,y)
+print'Power: ',power
---
+++ Bai9.py
@@ -0,0 +1,7 @@
+Multiple = 1 
+i = 1
+n = input('Enter Value: ')
+while ( i <= n ) : 
+	Multiple = Multiple * i 
+	i = i + 1
+print 'Multiple = ', Multiple
